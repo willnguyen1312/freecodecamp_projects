06f47842b7717686df66a8b09aabcc2b090f9929
Nam Nguyen <nam.nguyen.node@outlook.com>
2018-01-17 10:20:03 +0700

    Clean up

--- src/index.js
+++ src/index.js
@@ -2,12 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import "bootstrap/dist/css/bootstrap.min.css"
 import "weather-icons/css/weather-icons.min.css"
-import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
 
 if (module.hot) {
     module.hot.accept();
